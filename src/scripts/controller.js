2df21a138356c68fde5e446775a681c3d7a3a612
Daniel <daniel.moderiano@outlook.com>
2021-10-13 17:18:26 +1030

    Integrate timer into img clicks/chars found

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -8,11 +8,7 @@ const popupMenu = document.querySelector('.popup__list');
 let coordinates = {};
 
 const timer = Timer();
-const runningTimer = timer.begin();
-
-setTimeout(() => {
-  timer.end(runningTimer);
-}, 5000);
+let runningTimer;
 
 const getClickCoordinates = (e) => {
   const rect = e.target.getBoundingClientRect();
@@ -27,7 +23,6 @@ const checkForCharClick = (clickCoordinates) => {
   let charClicked = [];
   characters.forEach((character) => {
     if (character.isWithinBounds(clickCoordinates.top, clickCoordinates.left)) {
-      console.log(`${character.name} clicked`);
       if (!character.isFound()) {
         charClicked.push(character.name);
       }
@@ -48,13 +43,15 @@ popupMenu.addEventListener('click', (e) => {
   if (e.target.classList.contains('popup__list-item')) {
     if (charClicked.length > 0) {
       if (e.target.dataset.name === charClicked[0]) {
-        console.log(`Found ${e.target.dataset.name}!`);
         const charFound = characters.filter((char) => char.name === charClicked[0]);
         charFound[0].toggleFound();
         // Toggle UI on popup menu itself
         e.target.style.textDecoration = 'line-through';
         e.target.style.color = '#646464';
-        console.log(anyCharsRemaining());
+        if (!anyCharsRemaining()) {
+          timer.end(runningTimer);
+          console.log(timer.getFinalTime());
+        }
       } else {
         console.log('Miss');
       }
@@ -68,6 +65,9 @@ popupMenu.addEventListener('click', (e) => {
 window.addEventListener('click', (e) => {
   // All image-related functions should only be fired when the user actually clicks within the image boundaries.
   if (e.target === img) {    
+    if (!timer.isRunning()) {
+      runningTimer = timer.begin();
+    }
     coordinates = getClickCoordinates(e);
     displayReticleAtCursor(coordinates);
     displayPopupMenuAtCursor(coordinates);   
--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,9 +1,11 @@
 function Timer() {
-  const start = new Date().getTime();
   let elapsed = '0.0';
+  let running = false;
 
   // Run set interval function every 100 ms
   const begin = () => {
+    running = true;
+    const start = new Date().getTime();
     const runningTimer = setInterval(() => {
       // Reset the current time using the Date API and fixed start variable, rather than relying on accurate 100ms intervals with the setInterval function
       const time = new Date().getTime() - start;
@@ -24,9 +26,15 @@ function Timer() {
     clearInterval(interval);
   };
 
+  const getFinalTime = () => elapsed;
+
+  const isRunning = () => running;
+
   return {
     begin,
     end,
+    getFinalTime,
+    isRunning,
   }
 }
 
