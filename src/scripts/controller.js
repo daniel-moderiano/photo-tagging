f87053a259e9f84ff73ef6daa07ba268e19e862e
Daniel <daniel.moderiano@outlook.com>
2021-10-19 15:15:26 +1030

    Finalise controller module

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -37,36 +37,7 @@ const checkForCharClick = (clickCoordinates) => {
   return charClicked;
 };
 
-// Event propagation on popup ul element to catch user clicking on list item
-popupMenu.addEventListener('click', (e) => {
-  const charClicked = checkForCharClick(coordinates)
-  if (e.target.classList.contains('popup__list-item')) {
-    if (charClicked.length > 0) {
-      if (e.target.dataset.name === charClicked[0]) {
-        const charFound = characters.filter((char) => char.name === charClicked[0]);
-        charFound[0].toggleFound();
-        
-        toggleToast('Found one!', 'success');
-        // Toggle UI on popup menu itself
-        updatePopupMenu(e.target);
-        if (!anyCharsRemaining()) {
-          timer.end(runningTimer);
-          // Toggle complete-modal 
-          displayModal(completeModal);
-          document.querySelector('.modal__text-time').textContent = ` ${parseFloat(timer.getCurrentTime()).toFixed(1)} `;
-          document.querySelector('.modal__text-time').dataset.time = timer.getCurrentTime();
-        }
-      } else {
-        toggleToast('Hmm, not there...', 'error');
-      }
-    } else {
-      toggleToast('Hmm, not there...', 'error');
-    }
-    updateHeaderCards();
-  }
-});
-
-// Global event listener
+// Global event listener that listens specifically for image click before starting timer and setting other vars
 window.addEventListener('click', (e) => {
   // All image-related functions should only be fired when the user actually clicks within the image boundaries.
   if (e.target === img) {
@@ -86,7 +57,89 @@ window.addEventListener('click', (e) => {
   }
 });
 
+// Event propagation on popup ul element to catch user clicking on list item
+popupMenu.addEventListener('click', (e) => {
+  const charClicked = checkForCharClick(coordinates);
 
+  if (e.target.classList.contains('popup__list-item')) {
+    // Checks that the user's click is on the location of a character
+    if (charClicked.length > 0) {
+      // Checks that the user selects the matching popup menu item to the char clicked
+      if (e.target.dataset.name === charClicked[0]) {
+        // Toggle found status in codebase
+        const [charFound] = characters.filter((char) => char.name === charClicked[0]);
+        charFound.toggleFound();
+
+        // Update UI to reflect char found
+        toggleToast('Found one!', 'success');
+        updatePopupMenu(e.target);
+
+        // End game if all characters have been found
+        if (!anyCharsRemaining()) {
+          timer.end(runningTimer);
+          displayModal(completeModal);
+          document.querySelector('.modal__text-time').textContent = ` ${timer.getCurrentTime()} `;
+          document.querySelector('.modal__text-time').dataset.time = timer.getCurrentTime();
+        }
+      } else {
+        toggleToast('Hmm, not there...', 'error');
+      }
+    } else {
+      toggleToast('Hmm, not there...', 'error');
+    }
+    // Reflect any found characters by updating the header pictures
+    updateHeaderCards();
+  }
+});
+
+// Switching to leaderboard 'page' should reset the timer
+leaderboardBtn.addEventListener('click', () => {
+  if (timer.isRunning()) {
+    timer.end(runningTimer);
+    timer.reset();
+  }
+  switchToLeaderboard();
+});
+
+// Switching to home/image page should reset the timer and UI so the user can play again
+homeBtn.addEventListener('click', () => {
+  timer.end(runningTimer);
+  timer.reset();
+  // Alter found status on all characters, then update header cards and popup menu
+  characters.forEach((char) => {
+    if (char.isFound()) {
+      char.toggleFound();
+    }
+  });
+  updateHeaderCards();
+  removePopupStyling();
+  switchToImage();
+});
+
+// User clicks this button to submit their high score to the Firestore db
+submitBtn.addEventListener('click', () => {
+  // Get user data. Dataset is used to avoid having to trim spaces from the modal title time
+  const name = document.querySelector('#input-name').value;
+  const time = parseFloat(document.querySelector('.modal__text-time').dataset.time);
+
+  // Reject empty name field as firebase will accept this on the backend otherwise
+  if (name === "") {
+    toggleToast('Name cannot be left blank', 'error');
+    throw new Error('Must enter name');
+  }
+
+  // Give the user visual indication that the request is in progress
+  submitBtn.textContent = 'Submitting...';
+
+  // Call async function to add user to leaderboard, then close modal and switch page
+  addUser(name, time).then(() => {
+    submitBtn.textContent = 'Submit';
+    closeModal(completeModal);
+    switchToLeaderboard();
+  }) // .catch can be added here if necessary
+});
+
+// Avoid submitting high score, and instead return to the home/image page
 cancelBtn.addEventListener('click', () => {
   closeModal(completeModal);
   // Reset timer to zero but DO NOT start timer again here
@@ -99,49 +152,3 @@ cancelBtn.addEventListener('click', () => {
   removePopupStyling();
   switchToImage();
 });
-
-leaderboardBtn.addEventListener('click', () => {
-  closeModal(completeModal);
-  if (timer.isRunning()) {
-    timer.end(runningTimer);
-    timer.reset();
-  }
-  switchToLeaderboard();
-});
-
-homeBtn.addEventListener('click', () => {
-  timer.end(runningTimer);
-  timer.reset();
-
-  // Alter found status on all characters, then update header cards and popup menu
-  characters.forEach((char) => {
-    if (char.isFound()) {
-      char.toggleFound();
-    }
-  });
-  updateHeaderCards();
-  removePopupStyling();
-  switchToImage();
-});
-
-submitBtn.addEventListener('click', () => {
-  // Get user data
-  const name = document.querySelector('#input-name').value;
-  const time = parseFloat(document.querySelector('.modal__text-time').dataset.time);
-
-  if (name === "") {
-    toggleToast('Name cannot be left blank', 'error');
-    throw new Error('Must enter name');
-  }
-
-  submitBtn.textContent = 'Submitting...'
-  // Call async function to add user to leaderboard, then close modal
-  addUser(name, time).then(() => {
-    closeModal(completeModal);
-    submitBtn.textContent = 'Submit';
-    switchToLeaderboard();
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
